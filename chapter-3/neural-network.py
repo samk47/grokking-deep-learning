fd4db99293d5a8700293b537fa257735fa00cc0d
samuel <samkura47@gmail.com>
2018-11-17 16:12:02 +0300

    neural network with mulitple inputs.

--- chapter-3/neural-network.py
+++ chapter-3/neural-network.py
@@ -1,13 +1,23 @@
-weight = 0.1
+weight = [0.1, 0.2, 0]
 
 
 def neural_network(input, weight):
-    prediction = input * weight
+    prediction = w_sum(input, weight)
     return prediction
 
 
-number_of_toes = [8.5, 9.5, 10, 9]
+def w_sum(a, b):
+    assert (len(a) == len(b))
+    output = 0
+    for i in range(len(a)):
+        output += (a[i] * b[i])
+    return output
 
-input = number_of_toes[0]
-prediction = neural_network(weight, input)
+
+toes = [8.5, 9.5, 10, 9]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+input = [toes[0], wlrec[0], nfans[0]]
+prediction = neural_network(input, weight)
 print(prediction)
