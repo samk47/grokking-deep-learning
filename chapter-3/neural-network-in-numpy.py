a43da361328ff707deb611c81670a9310b4fcf13
samuel <samkura47@gmail.com>
2018-11-17 16:41:34 +0300

    neural network using numpy for the dot product (weighted sum)

---
+++ chapter-3/neural-network-in-numpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+weight = np.array([0.1, 0.2, 0])
+
+
+def neural_network(input, weights):
+    pred = input.dot(weights)
+    return pred
+
+
+toes = np.array([8.5, 9.5, 10, 9])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
+
+input = np.array([toes[0], wlrec[0], nfans[0]])
+
+prediction = neural_network(input, weight)
+print(prediction)
